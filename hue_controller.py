4e73b14ab139ad2657b2b956ef3a61a19a474d32
E. Almqvist <elalmqvist@gmail.com>
2020-08-03 23:13:27 +0200

    Async requests

--- hue_controller.py
+++ hue_controller.py
@@ -1,15 +1,20 @@
 import requests as req # Used for HTTP requests for the Hue API
 import json # API uses JSON
-import config # Configuration for the controller
+import config # Configuration for the controller (/config.py <- change this file)
 
 def genUrl(params: str):
-    return "https://" + config.address + "/api/" + config.username + params
+    return "http://" + config.address + "/api/" + config.username + params
 
-def API_Request(ReqType: str, params: str):
-    apiReq = req.Request( ReqType, genUrl(params) )
+await def API_Request(ReqType: str="GET", params: str=""): # Requests for the API
+    try:
+        apiReq = req.Request( ReqType, genUrl(params) )
+        apiReqPrep = apiReq.prepare()
 
-    apiReqPrep = apiReq.prepare()
-    apiSession = req.Session()
-    apiSession.send(apiReqPrep)
+        apiSession = req.Session()
+        response = apiSession.send(apiReqPrep)
+
+        print(response.body)
+    except req.exceptions.RequestException as err:
+        raise SystemExit(err) # throw the error
 
 
--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 #!/usr/bin/env python
 from lib.input import * # Commandline parser
 
-import hue_controller # Actual controller 
+import hue_controller as hue # Actual controller 
+
+params = ""
+if( inputHasKeys(["-p"]) ):
+    params = getValueOfKey("-p")
+
+reqtype = "GET"
+if( inputHasKeys(["-t"]) ):
+    reqtype = getValueOfKey("-t")
+
 
 def init():
-
+    print(reqtype, params)
+    hue.API_Request(reqtype, params)
 
 init()
