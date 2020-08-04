add4ea65394caa47585c4d8959e6d4346dcacb2d
E. Almqvist <elalmqvist@gmail.com>
2020-08-04 20:19:21 +0200

    Began work on remote

--- presets.py
+++ presets.py
@@ -1,7 +1,29 @@
 # Presets goes in here
 PRESETS = {
+
     "default": {
         "color": (178, 199, 255),
         "brightness": 255
+    },
+
+    "red": {
+        "color": (255, 0, 0),
+        "brightness": 255
+    },
+
+    "green": {
+        "color": (0, 255, 0),
+        "brightness": 255
+    },
+
+    "blue": {
+        "color": (0, 0, 255),
+        "brightness": 255
+    },
+
+    "sleep": {
+        "color": (185, 155, 25),
+        "brightness": 80
     }
+
 }
