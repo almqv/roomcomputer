f12b6dbd6fbd817b4ef7d74614f56f1993381fc3
E. Almqvist <elalmqvist@gmail.com>
2020-08-04 14:03:28 +0200

    RGB Bug fix

--- lib/func.py
+++ lib/func.py
@@ -9,13 +9,13 @@ def boolToString(v: bool): # To fix the dumb python syntax
     return boolStr[v]
 
 def rgbToDecimal( r:int, g:int, b:int ):
-    return round(r/255), round(g/255), round(b/255)
+    return round(r/255, 1), round(g/255, 1), round(b/255, 1)
 
 def svNumFix(n: float):
     return int(round(n*254, 0))
 
 def hueNumFix(n: float):
-    return int(round(n*65535))
+    return int(round(n*65535, 0))
 
 def rgbToHsv( r:int, g:int, b:int ):
     R, G, B = rgbToDecimal(r, g, b)
