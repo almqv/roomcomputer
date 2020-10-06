085bf39b29eed14c602a9efeae3765abc1451473
E. Almqvist <h1g.server@gmail.com>
2020-10-06 18:26:01 +0200

    Everything costs money sadly

--- speech/speech.py
+++ speech/speech.py
@@ -5,8 +5,28 @@ class voiceInput(object):
 
 	muted = True
 
+	def voiceToText( self, deviceIndex=30 ):
+		try:
+			with sr.Microphone( deviceIndex ) as src:
+				self.recognizer.adjust_for_ambient_noise( src, 0.2 )
+				audio = self.recognizer.listen( src )
+				text = self.recognizer.recognize_google(audio)
+				print(text)
+
+		except sr.RequestError as err:
+			print("Unable to request results: {0}".format(err))
+
+		except sr.UnknownValueError as err:
+			print("Unknown Error: {0}".format(err))
+
+
 	def setMuted( self, setm: bool=True ):
 		self.muted = setm
 
 	def switchMute( self ):
 		self.setMuted( not self.muted )
+
+
+voice = voiceInput()
+voice.setMuted(False)
+print( "out:", voice.voiceToText() )
