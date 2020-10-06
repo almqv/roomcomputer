2082cee4e07e1da4747243afb276d08400cd82d2
E. Almqvist <h1g.server@gmail.com>
2020-10-06 22:11:06 +0200

    Fixed config loading bugs

--- modules/hue/config.py
+++
@@ -1,4 +0,0 @@
-# Hue Remote Settings
-class hue_config:
-	address = "192.168.0.3"
-	username = "E0ru0AeVFKEH1E30X40JAJfovg4Uu1aTkdrKQ2Oi"
--- modules/hue/default-config.py
+++
@@ -1,8 +0,0 @@
-##################################
-# RENAME THIS FILE TO "config.py"#
-##################################
-
-# Hue Remote Settings
-class hue_config:
-	address = "" # Local IPv4 address to the HUE bridge
-	username = "" # Username for the bridge
