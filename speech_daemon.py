5c3efe66b1fea3bd97a2edd61bd15dbe1df40201
E. Almqvist <elalmqvist@gmail.com>
2020-10-06 19:32:16 +0200

    Added daemon

--- speech_daemon.py
+++ speech_daemon.py
@@ -1,3 +1,17 @@
 #!/usr/bin/env python
 
-import lib.speech.speech
+from modules.hue.hue_remote import parseCommand
+from modules.speech.speech import voiceInput
+
+class speech_daemon(object):
+	voiceInpObj = None
+
+	def __init__(self, deviceIndex=30):
+		voiceInpObj = voiceInput()
+
+		voiceInpObj.setMuted(False)
+
+		voiceInpObj.start(deviceIndex)
+
+if __name__ == "__main__":
+	daemon = speech_daemon()
