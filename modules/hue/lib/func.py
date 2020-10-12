ea87ee293cebb90abe2945b4d968ade3456b9685
Alve <alve@hotmail.se>
2020-10-12 11:11:45 +0200

    Revert "Made code pep8 compliant once again"
    This reverts commit c0ad5ce2d70464cb28c00cdf97b9eb8357d4b040.

--- modules/hue/lib/func.py
+++ modules/hue/lib/func.py
@@ -5,24 +5,19 @@ boolStr = {
     False: "false"
 }
 
-
-def boolToString(v: bool):  # To fix the dumb python syntax
+def boolToString(v: bool): # To fix the dumb python syntax
     return boolStr[v]
 
-
-def rgbToDecimal(r: int, g: int, b: int):
+def rgbToDecimal( r:int, g:int, b:int ):
     return round(r/255, 1), round(g/255, 1), round(b/255, 1)
-
 
 def svNumFix(n: float):
     return int(round(n*254, 0))
 
-
 def hueNumFix(n: float):
     return int(round(n*65535, 0))
 
-
-def rgbToHsv(r: int, g: int, b: int):
+def rgbToHsv( r:int, g:int, b:int ):
     R, G, B = rgbToDecimal(r, g, b)
     H, S, V = colorsys.rgb_to_hsv(R, G, B)
     return hueNumFix(H), svNumFix(S), svNumFix(V)
--- speech_daemon.py
+++ speech_daemon.py
@@ -13,43 +13,41 @@ homedir = expanduser("~")
 
 CONFIG = {}
 
-
 class speech_daemon(object):
-    voiceInpObj = None
-    deviceIndex = 30
+	voiceInpObj = None
+	deviceIndex = 30
 
-    def __init__(self, deviceIndex=30):
-        self.voiceInpObj = voiceInput()
-        self.voiceInpObj.setMuted(False)
+	def __init__(self, deviceIndex=30):
+		self.voiceInpObj = voiceInput()
+		self.voiceInpObj.setMuted(False)
 
-        self.deviceIndex = deviceIndex
+		self.deviceIndex = deviceIndex
 
-    def loadconfig(self):
-        path = homedir + "/.config/roomcomputer/config.json"
-        # if no config path is
-        # specified then choose the users default
+	def loadconfig(self):
+		path = homedir + "/.config/roomcomputer/config.json"
+		# if no config path is 
+		# specified then choose the users default
 
-        if(len(sys.argv) > 1):
-            path = sys.argv[1]
+		if( len(sys.argv) > 1 ):
+			path = sys.argv[1]
 
-        cfg = readconfig(path)  # read the config
+		cfg = readconfig(path) # read the config
 
-        global CONFIG
-        CONFIG = cfg
+		global CONFIG
+		CONFIG = cfg
 
-    def start(self):
-        controller.init()
+	def start(self):
+		controller.init()
 
-        for inp in self.voiceInpObj.start(self.deviceIndex):
-            cmdBuf = inp.lower().split(" ")
-            if(cmdBuf[0] in CONFIG["speech"]["prefixes"]):
-                print("CMD:", cmdBuf)
-                parseCommandline(cmdBuf, False)
+		for inp in self.voiceInpObj.start( self.deviceIndex ):
+			cmdBuf = inp.lower().split(" ")
+			if( cmdBuf[0] in CONFIG["speech"]["prefixes"] ):
+				print("CMD:", cmdBuf)
+				parseCommandline( cmdBuf, False )
 
-        controller.end()
-
+		controller.end()
 
 if __name__ == "__main__":
-    daemon = speech_daemon()
-    daemon.loadconfig()
-    daemon.start()
+	daemon = speech_daemon()
+	daemon.loadconfig()
+	daemon.start()
