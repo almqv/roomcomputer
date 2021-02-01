4b50fe5554813009a348252fffa9806e01682206
E. Almqvist <elalmqvist@gmail.com>
2021-02-01 15:33:16 +0100

    Install script fix

--- install.sh
+++ install.sh
@@ -2,11 +2,18 @@
 
 repoPath=$(dirname "$(realpath $0)")
 
+# Setup config stuff
+$repoPath/setup.sh
+
+
+# Install the stuff
 echo Creating roomcomputer symlink in /opt/...
+sudo rm /opt/roomcomputer 
 sudo ln -s $repoPath /opt/roomcomputer
 sudo pip install -r /opt/roomcomputer/requirements.txt
 
 echo Creating hue_cmd.py symlink in /usr/bin/...
-sudo ln -s /opt/roomcomputer/hue_cmd.py  /usr/bin/hue
+sudo rm /usr/bin/hue
+sudo ln -s /opt/roomcomputer/hue_cmd.py /usr/bin/hue
 
 echo Done.
