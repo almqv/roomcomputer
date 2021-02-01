e12ef3c9039f1570c88615dc8f6f944623f30510
E. Almqvist <elalmqvist@gmail.com>
2021-02-01 15:43:16 +0100

    Error message

--- setup.sh
+++ setup.sh
@@ -2,6 +2,10 @@
 
 cfgPath="$HOME/.config/roomcomputer"
 
-mkdir $cfgPath
-cp default-config.json $cfgPath/config.json
-cp default-presets.json $cfgPath/presets.json
+if [ -d $cfgPath ]; then
+	echo Config already exists.
+else
+	mkdir $cfgPath
+	cp default-config.json $cfgPath/config.json
+	cp default-presets.json $cfgPath/presets.json
+fi
